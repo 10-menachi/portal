f2553d86edcabb3b0a3c4343b758a10f0723d65f
chrisdroid <bobitlmr188221@spu.ac.ke>
2024-07-30 16:06:29 +0300

    Edit AdminModel and TermTaxonomySeeder

--- app/Database/Seeds/TermTaxonomySeeder.php
+++ app/Database/Seeds/TermTaxonomySeeder.php
@@ -8,6 +8,14 @@ class TermTaxonomySeeder extends Seeder
 {
     public function run()
     {
-        //
+        $data = [
+            [
+                'term_id' => 1,
+                'taxonomy' => 'product_cat',
+            ],
+        ];
+
+        // Using Query Builder
+        $this->db->table('term_taxonomy')->insertBatch($data);
     }
 }
